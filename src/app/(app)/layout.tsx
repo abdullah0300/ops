56d58e7a51d68677da8ad0b5c1250793c7899e3d
Abdullah <aslamabdullah724@gmail.com>
2026-03-16 20:28:24 +0500

    adedd fully

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -41,6 +41,15 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
             gtag('config', 'G-C1MF00750G');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vwq3cksr8j");
+          `}
+        </Script>
       </head>
       <body>
         <Providers>
